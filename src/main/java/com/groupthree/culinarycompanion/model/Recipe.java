ae392df83720681c12312b0431032ee0a66603d0
saif-bayyari <saifbayyari@protonmail.com>
2023-10-10 16:24:46 -0400

    saif's branch initial commit

---
+++ src/main/java/com/groupthree/culinarycompanion/model/Recipe.java
@@ -0,0 +1,18 @@
+package com.groupthree.culinarycompanion.model;
+
+public class Recipe {
+
+    private int id;
+    private String name;
+
+    private String cuisine; //ex: Mexican cuisine, Italian Cuisine, Chinese cuisine, etc
+
+    private String type;
+
+    private String difficulty;
+
+    private List<Ingredients>();
+
+    private
+
+}
---
+++ src/test/java/com/groupthree/culinarycompanion/CulinarycompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupthree.culinarycompanion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulinarycompanionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
