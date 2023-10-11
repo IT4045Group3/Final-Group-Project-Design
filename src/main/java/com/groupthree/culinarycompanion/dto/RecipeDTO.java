c77ba852c901ad9c1ed1b1c953d8eccb32018a3d
Yifan Bian <bianya@mail.uc.edu>
2023-10-10 21:28:14 -0400

    add DTO layer

---
+++ src/main/java/com/groupthree/culinarycompanion/dto/RecipeDTO.java
@@ -0,0 +1,13 @@
+package com.groupthree.culinarycompanion.dto;
+
+public class RecipeDTO {
+    private long recipeId;
+    private String name;
+    private String cuisine;
+    private String type;
+    private String difficulty;
+    private List<Ingredient> ingredients;
+    private List<Instruction> instructions;
+
+    // Getters and setters
+}
---
+++ src/main/java/com/groupthree/culinarycompanion/dto/UserDTO.java
@@ -0,0 +1,9 @@
+package com.groupthree.culinarycompanion.dto;
+
+public class UserDTO {
+    private long userId;
+    private String username;
+    private String email;
+
+    // Getters and setters
+}
