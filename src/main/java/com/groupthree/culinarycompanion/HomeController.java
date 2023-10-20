9301c3a86f77540d49341f158732d96c19e46f10
Yifan Bian <bianya@mail.uc.edu>
2023-10-20 19:25:43 -0400

    update home controller to add search function

--- src/main/java/com/groupthree/culinarycompanion/HomeController.java
+++ src/main/java/com/groupthree/culinarycompanion/HomeController.java
@@ -1,6 +1,9 @@
 package com.groupthree.culinarycompanion;
 
 
+import com.groupthree.culinarycompanion.dao.RecipeDAOStub;
+import com.groupthree.culinarycompanion.dto.RecipeDTO;
+import com.groupthree.culinarycompanion.model.Recipe;
 import com.groupthree.culinarycompanion.service.CuisineCategoryService;
 import com.groupthree.culinarycompanion.service.RecipeService;
 import jakarta.servlet.http.HttpSession;
@@ -8,6 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -28,6 +34,15 @@ public class HomeController {
     public String home(Model model) {
         model.addAttribute("cuisineCategories", cuisineCategoryService.getAllCuisineCategories());
         model.addAttribute("recipes", recipeService.getAllRecipes());
+        return "home";
+    }
+
+    @GetMapping("/search")
+    public String search(@RequestParam("keyword") String keyword, Model model) {
+        model.addAttribute("cuisineCategories", cuisineCategoryService.getAllCuisineCategories());
+        model.addAttribute("recipes", recipeService.getAllRecipes());
+        List<RecipeDTO> searchResults = recipeService.findRecipesByNameContaining(keyword);
+        model.addAttribute("searchResults", searchResults);
         return "home";
     }
 
